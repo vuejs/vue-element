343860262f36173a17e641b1a6340a17f62ae758
Karol Fabjańczuk <karol.fabjanczuk@gmail.com>
2017-01-04 01:07:29 +0100

    feat(vue-element): fix 'this' in es2015 constructor

--- src/utils/registerCustomElement.js
+++ src/utils/registerCustomElement.js
@@ -3,34 +3,42 @@ import isES2015 from './isES2015';
 import 'set-prototype-of'; //polyfill for older browsers (IE < 11) that do not support Object.setPrototypeOf needed by Webpack's transpiled ES2015 "class", even if it's not directly used
 
 export default function registerCustomElement(name, options = {}) {
+  function connectedCallback() {
+    typeof options.connectedCallback === 'function' && options.connectedCallback.call(this);
+  };
+
+  function disconnectedCallback() {
+    typeof options.disconnectedCallback === 'function' && options.disconnectedCallback.call(this);
+  };
+
+  function attributeChangedCallback(name, oldValue, value) {
+    typeof options.attributeChangedCallback === 'function' && options.attributeChangedCallback.call(this, name, oldValue, value);
+  };
+
   if (isES2015) {
     // ES2015 detected. We will use "class" based Custom Elements V1 specification. If it's natively supported it will run without polyfill
     class myCustomElement extends HTMLElement {
       // Can define constructor arguments if you wish.
       constructor(self) {
         super();
-        self = HTMLElement.call(self || this);
+        if (self) {
+          self = HTMLElement.call(self);
+        } else {
+          self = this;
+        }
 
         typeof options.constructorCallback === 'function' && options.constructorCallback.call(self);
         return self;
       }
 
-      connectedCallback() {
-        typeof options.connectedCallback === 'function' && options.connectedCallback.call(this);
-      }
-
-      disconnectedCallback() {
-        typeof options.disconnectedCallback === 'function' && options.disconnectedCallback.call(this);
-      };
-
       static get observedAttributes() {
         return options.observedAttributes || [];
       }
-
-      attributeChangedCallback(name, oldValue, value) {
-        typeof options.attributeChangedCallback === 'function' && options.attributeChangedCallback.call(this, name, oldValue, value);
-      }
     }
+
+    myCustomElement.prototype.connectedCallback = connectedCallback;
+    myCustomElement.prototype.disconnectedCallback = disconnectedCallback;
+    myCustomElement.prototype.attributeChangedCallback = attributeChangedCallback;
 
     customElements.define(name, myCustomElement);
   } else {
@@ -54,17 +62,9 @@ export default function registerCustomElement(name, options = {}) {
       }
     );
 
-    myCustomElement.prototype.connectedCallback = function () {
-      typeof options.connectedCallback === 'function' && options.connectedCallback.call(this);
-    };
-
-    myCustomElement.prototype.disconnectedCallback = function () {
-      typeof options.disconnectedCallback === 'function' && options.disconnectedCallback.call(this);
-    };
-
-    myCustomElement.prototype.attributeChangedCallback = function (name, oldValue, value) {
-      typeof options.attributeChangedCallback === 'function' && options.attributeChangedCallback.call(this, name, oldValue, value);
-    };
+    myCustomElement.prototype.connectedCallback = connectedCallback;
+    myCustomElement.prototype.disconnectedCallback = disconnectedCallback;
+    myCustomElement.prototype.attributeChangedCallback = attributeChangedCallback;
 
     customElements.define(name, myCustomElement);
   }
