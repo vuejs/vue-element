ac2aa9a44b532e7d70f675cd2824e29709399e1e
Karol Fabjańczuk <karol.fabjanczuk@gmail.com>
2017-02-18 23:33:53 +0100

    test: minor cleanup

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -5,7 +5,6 @@ var config = require('../../config')
 module.exports = {
   src_folders: ['test/e2e/specs'],
   output_folder: 'test/e2e/reports',
-  custom_assertions_path: ['test/e2e/custom-assertions'],
 
   selenium: {
     start_process: true,
