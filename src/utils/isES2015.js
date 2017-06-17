34939f1d63a32f8b1579dc40761307ff339e3df2
Karol Fabjańczuk <karol.fabjanczuk@gmail.com>
2017-06-17 22:48:09 +0200

    fix(#26): Avoid using eval

--- src/utils/isES2015.js
+++ src/utils/isES2015.js
@@ -1,8 +1,5 @@
 function isES2015() {
   if (typeof Symbol === 'undefined' || typeof Reflect === 'undefined') return false;
-  try {
-    eval('class Foo {}'); // eslint-disable-line no-eval
-  } catch (e) { return false; }
 
   return true;
 }
